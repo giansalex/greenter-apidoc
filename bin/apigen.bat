47892da2dfe9de9aea4c155ec7bfc08ae8fc68c7
giansalex <giansalex@gmail.com>
2018-11-15 20:02:29 -0500

    up memory

--- bin/apigen.bat
+++ bin/apigen.bat
@@ -2,4 +2,4 @@ REM COPY TO vendor/bin
 @ECHO OFF
 setlocal DISABLEDELAYEDEXPANSION
 SET BIN_TARGET=%~dp0/../apigen/apigen/bin/apigen
-php -d memory_limit=512M "%BIN_TARGET%" %*
+php -d memory_limit=1024M "%BIN_TARGET%" %*
