a3877a75237fe891dbda5589504488d4dfff43f5
Giancarlos Salas <giansalex@gmail.com>
2020-08-08 19:23:12 -0500

    Configure greenter theme

--- config.php
+++ config.php
@@ -12,8 +12,10 @@ $iterator = Finder::create()
 ;
 
 return new Sami($iterator, array(
+    'theme'                => 'greenter',
     'title'                => 'Greenter API',
     'build_dir'            => __DIR__.'/build',
     'cache_dir'            => __DIR__.'/cache',
+    'template_dirs'        => array(__DIR__.'/themes/greenter'),
     'default_opened_level' => 2,
 ));
